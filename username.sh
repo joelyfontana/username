c9e5f3105725f08d914be118dff543a999e3beb9
Joely Fontana <fontana@chapman.edu>
2018-11-09 11:47:47 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -1,8 +1,10 @@
+#Joely Fontana
 #! /bin/bash
 # username.sh
+echo "Your username must be between 3-12 characters, begin with a lower case letter, and can only contain lowercase letters, digits, and an underscore." 
 echo "Enter a username: "
 read USERNAME
-while echo $USERNAME | egrep -v "^[a-z][a-z0-9_]{3,11}$" > /dev/null 2>&1
+while echo $USERNAME | egrep -v "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username - lower case only!"
 	echo "Enter a username: "
