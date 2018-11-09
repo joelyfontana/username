a0ead6c43f8384d332ca2536aeb79ebd9f37a3a3
Joely Fontana <fontana@chapman.edu>
2018-11-09 11:38:54 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -2,7 +2,7 @@
 # username.sh
 echo "Enter a username: "
 read USERNAME
-while echo $USERNAME | egrep -v "^[a-z0-9_]{3,12}$" > /dev/null 2>&1
+while echo $USERNAME | egrep -v "^[a-z][a-z0-9_]{3,11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username - lower case only!"
 	echo "Enter a username: "
